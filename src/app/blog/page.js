8a2de666893bbc1ea60cf120b6f479c5b9f68d25
mohammad2rr <mohammad2rghdd@gmail.com>
2025-01-17 14:02:47 +0330

    added styles and components to cat and sub cat

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,6 +1,3 @@
-import { Button } from "primereact/button";
-import { Accordion, AccordionTab } from "primereact/accordion";
-
 import Breadcrumb from "@/components/modules/breadcrumb/Breadcrumb";
 import Footer from "@/components/modules/footer/Footer";
 import Navbar from "@/components/modules/navbar/Navbar";
@@ -15,25 +12,7 @@ const page = async () => {
       <Breadcrumb route={" مقالات"} />
       <div className={styles.container} data-aos="fade-up">
         <h1>page blog</h1>
-        <div className="card">
-          <Accordion activeIndex={0}>
-            <AccordionTab header="Header I">
-              <p className="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              </p>
-            </AccordionTab>
-            <AccordionTab header="Header II">
-              <p className="m-0">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              </p>
-            </AccordionTab>
-            <AccordionTab header="Header III">
-              <p className="m-0">
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui
-              </p>
-            </AccordionTab>
-          </Accordion>
-        </div>
+        <div className="card"></div>
       </div>
       <Footer />
     </>
--- src/app/p-admin/products-category/page.js
+++ src/app/p-admin/products-category/page.js
@@ -1,19 +1,29 @@
 import React from "react";
 import Layout from "@/components/layouts/AdminPanelLayout";
-import styles from "@/components/templates/p-admin/products/table.module.css";
-import Table from "@/components/templates/p-admin/products/Table";
 import connectToDB from "@/configs/db";
-import ProductModel from "@/models/Product";
-import AddProduct from "@/components/templates/p-admin/products/AddProduct";
+
+import { Accordion, AccordionTab } from "primereact/accordion";
 
 const page = async () => {
   connectToDB();
-  const products = await ProductModel.find({}).sort({ _id: -1 }).lean();
 
   return (
     <Layout>
       <main>
-        <p>products category</p>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <Accordion activeIndex={0}>
+                <AccordionTab header="ایجاد دسته بندی محصولات">
+                  <p className="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do
+                  </p>
+                </AccordionTab>
+              </Accordion>
+            </div>
+          </div>
+        </div>
       </main>
     </Layout>
   );
